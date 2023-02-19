3e910e0e60e0bd3977532f7953064bca5202b4c0
BernardSELASI <asamoahbselasi@gmail.com>
2023-02-19 07:57:46 -0800

    my positive and negative commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - entrypoint
- * Description: prints if a number is positive or negative
- * Retuen: Always 0.
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
  */
 int main(void)
 {
 	int n;
 
-	srand(tme(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < 0)
-	{
-		ptintf("%d is negative\n", n);
-	}
-	else
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
+
 
